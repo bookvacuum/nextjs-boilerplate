b57a7e5dec7714b49ac5750267f4cc560822f55a
Sunwoo Lee <slee20@stanford.edu>
2024-04-07 00:43:18 -0400

    edited video

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,30 @@
 import Image from "next/image";
 import localFont from "next/font/local";
+import React, { useRef, useEffect } from "react";
 
 // Font files can be colocated inside of `app`
 const myFont = localFont({
-  src: "./my-font.woff2",
+  src: "./milkman.ttf",
   display: "swap",
 });
 
 export default function Home() {
   return (
     <div>
-      <div className="video-overlay video-overlay2">
-        <video width="100%" height="100" autoPlay muted src="/video.mp4" />
+      <div className="video-overlay">
+        <video
+          width="100%"
+          height="50vh"
+          autoPlay
+          loop
+          muted
+          src="/video.mp4"
+
+          // className="transform rotate-180"
+        />
+      </div>
+      <div style={{ background: "rgba(17, 13, 17, 1)", height: "100rem" }}>
+        blah blah
       </div>
     </div>
   );
