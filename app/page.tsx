c6e18ef9ca331b28e5f8c02c5ec8822c1d45fc24
Sunwoo Lee <slee20@stanford.edu>
2024-04-07 16:56:04 -0400

    add technical specification dummy

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,22 @@ import Image from "next/image";
 import React, { useRef, useEffect } from "react";
 import localFont from "next/font/local";
 
-// Font files can be colocated inside of `app`
-
+const features = [
+  { name: "Origin", description: "Designed by Good Goods, Inc." },
+  {
+    name: "Material",
+    description:
+      "Solid walnut base with rare earth magnets and powder coated steel card cover",
+  },
+  { name: "Dimensions", description: '6.25" x 3.55" x 1.15"' },
+  { name: "Finish", description: "Hand sanded and finished with natural oil" },
+  { name: "Includes", description: "Wood card tray and 3 refill packs" },
+  {
+    name: "Considerations",
+    description:
+      "Made from natural materials. Grain and color vary with each item.",
+  },
+];
 export default function Home() {
   return (
     <div>
@@ -45,10 +59,57 @@ export default function Home() {
       <div
         style={{
           background: "rgba(17, 13, 17, 1)",
-          color: "white",
         }}
       >
-        blah blah
+        <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+          <div>
+            <h2 className="text-3xl font-bold tracking-tight text-[#c0c0c0] sm:text-4xl">
+              Technical Specifications
+            </h2>
+            <p className="mt-4 text-gray-400">
+              Imagenet is a platform dedicated to decentralizing AI training
+              data. Contributors are compensated through smart contracts,
+              guaranteeing fair and prompt payment. All of our data is stored in
+              peer to peer decentralized storage.
+            </p>
+
+            <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
+              {features.map((feature) => (
+                <div
+                  key={feature.name}
+                  className="border-t border-gray-300 pt-4"
+                >
+                  <dt className="font-medium text-gray-300">{feature.name}</dt>
+                  <dd className="mt-2 text-sm text-gray-400">
+                    {feature.description}
+                  </dd>
+                </div>
+              ))}
+            </dl>
+          </div>
+          <div className="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
+            <img
+              src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-01.jpg"
+              alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
+              className="rounded-lg bg-gray-100"
+            />
+            <img
+              src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-02.jpg"
+              alt="Top down view of walnut card tray with embedded magnets and card groove."
+              className="rounded-lg bg-gray-100"
+            />
+            <img
+              src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-03.jpg"
+              alt="Side of walnut card tray with card groove and recessed card area."
+              className="rounded-lg bg-gray-100"
+            />
+            <img
+              src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-04.jpg"
+              alt="Walnut card tray filled with cards and card angled in dedicated groove."
+              className="rounded-lg bg-gray-100"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
