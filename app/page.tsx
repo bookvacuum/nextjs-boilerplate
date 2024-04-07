56c03f09fe92a22307e1833c24f4cc3466079162
Sunwoo Lee <slee20@stanford.edu>
2024-04-06 23:39:20 -0400

    add font

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
+import localFont from "next/font/local";
+
+// Font files can be colocated inside of `app`
+const myFont = localFont({
+  src: "./my-font.woff2",
+  display: "swap",
+});
 
 export default function Home() {
   return (
     <div>
       <div className="video-overlay video-overlay2">
-        <video width="100%" height="100" loop autoPlay muted src="/video.mp4" />
+        <video width="100%" height="100" autoPlay muted src="/video.mp4" />
       </div>
     </div>
   );
